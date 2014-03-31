90dbe2c4440b5807e87ab99a5814c549dbf38f35
EvertLagerberg <evert.lagerberg@gmail.com>
2014-03-31 19:25:38 +0200

    Lagt till infotext på markeringarna

--- initialize.js
+++ initialize.js
@@ -21,22 +21,61 @@ function initialize() {
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  var marker = new google.maps.Marker({
+
+
+  var contentString1 = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h2 id="firstHeading" class="firstHeading">Area51</h2>'+
+      '<div id="bodyContent">'+
+      '<p>Aliens where here!</p>'+
+  
+      '</div>'+
+      '</div>';
+
+  var contentString2 = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h2 id="firstHeading" class="firstHeading">Landing site</h2>'+
+      '<div id="bodyContent">'+
+      '<p>The aliens landed somewhere out in the desert...!</p>'+
+  
+      '</div>'+
+      '</div>';  
+
+  var infowindow1 = new google.maps.InfoWindow({
+    content: contentString1,
+    maxWidth: 200
+  });
+
+  var infowindow2 = new google.maps.InfoWindow({
+    content: contentString2,
+    maxWidth: 200
+  });
+
+  var marker1 = new google.maps.Marker({
     position: Area51,
     map: map,
     draggable: false,
     animation: google.maps.Animation.DROP,
-    title: "Hello World!"
+    title: "Area51"
   });
 
 
-  var marker = new google.maps.Marker({
+  var marker2 = new google.maps.Marker({
     position: UFO,
     map: map,
     draggable: true,
     animation: google.maps.Animation.BOUNCE,
-    title: "Hello World!"
+    title: "UFO Landing site"
   });
+
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow1.open(map,marker1);
+  })
+  google.maps.event.addListener(marker2, 'click', function() {
+    infowindow2.open(map,marker2);
+  })     
 
 
 }
