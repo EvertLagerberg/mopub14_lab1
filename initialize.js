df7ea665c05d577eefec30cd8039e44a3c615f36
EvertLagerberg <evert.lagerberg@gmail.com>
2014-03-31 09:56:17 +0200

    First

---
+++ initialize.js
@@ -0,0 +1,46 @@
+var map;
+
+function initialize() {
+  var Area51 = new google.maps.LatLng(37.236752, -115.801813);
+  var UFO = new google.maps.LatLng(37.260752, -115.801813);
+  var mapOptions = {
+    zoom: 13,
+    center: Area51,
+    mapTypeId: 'satellite',
+    disableDefaultUI: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      position: google.maps.ControlPosition.RIGHT_TOP
+    },
+    panControl: true,
+    panControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_TOP
+    }
+  }
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: Area51,
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    title: "Hello World!"
+  });
+
+
+  var marker = new google.maps.Marker({
+    position: UFO,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.BOUNCE,
+    title: "Hello World!"
+  });
+
+
+}
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
