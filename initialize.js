4921a86053afb92b8b5e29ddbeba53ea9ad66e68
EvertLagerberg <evert.lagerberg@gmail.com>
2014-04-02 10:38:07 +0200

    Added css file

--- initialize.js
+++ initialize.js
@@ -13,176 +13,172 @@ console.log(KTH);
       console.log("Hej");
       var geo = new google.maps.LatLng(position.coords.latitude,
        position.coords.longitude);
-      
-})};
-
-function goTo(div,location) {
-
-
-  google.maps.event.addDomListener(div, 'click', function() {
-    var newlocation = location;
-    map.setCenter(newlocation);
-    map.setZoom(13);
-  });
-function goHome() {
-    var Home = new google.maps.LatLng(37.236752, -115.801813);
-    map.setCenter(Home);
-  }
-  
-function goKTH() {
-    var Home = new google.maps.LatLng(59.34694918843402, 18.07322323322296);
-    map.setCenter(Home);
-  }
-
-}
-function zoomin() {
-   var currentZoomLevel = map.getZoom();
-   if(currentZoomLevel != 0){
-     map.setZoom(currentZoomLevel + 1);}   
-  }
-
-function zoomout() {
-   var currentZoomLevel = map.getZoom();
-   if(currentZoomLevel != 0){
-     map.setZoom(currentZoomLevel - 1);}   
-  }
-  
-function goUp() {
-   var oldLoc = map.getCenter();
-   var newLoc = new google.maps.LatLng(oldLoc.lat() + 0.05, oldLoc.lng());
-   map.setCenter(newLoc);   
-  }
-  
-function goDown() {
-   var oldLoc = map.getCenter();
-   var newLoc = new google.maps.LatLng(oldLoc.lat() - 0.05, oldLoc.lng());
-   map.setCenter(newLoc);   
-  }
-
-function goLeft() {
-   var oldLoc = map.getCenter();
-   var newLoc = new google.maps.LatLng(oldLoc.lat(), oldLoc.lng() - 0.05);
-   map.setCenter(newLoc);   
-  }
-  
-function goRight() {
-   var oldLoc = map.getCenter();
-   var newLoc = new google.maps.LatLng(oldLoc.lat(), oldLoc.lng() + 0.05);
-   map.setCenter(newLoc);   
-  }
- 
- 
-
-function initialize() {
-  var mapOptions = {
-    zoom: 13,
-    center: Area51,
-    mapTypeId: 'satellite',
-    disableDefaultUI: true,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-      position: google.maps.ControlPosition.LEFT_TOP
     }
-  }
+    )
+  };
 
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-
-
-  var contentString1 = '<div id="content">'+
-  '<div id="siteNotice">'+
-  '</div>'+
-  '<h2 id="firstHeading" class="firstHeading">Area51</h2>'+
-  '<div id="bodyContent">'+
-  '<p>Aliens where here!</p>'+
-  
-  '</div>'+
-  '</div>';
-
-  var contentString2 = '<div id="content">'+
-  '<div id="siteNotice">'+
-  '</div>'+
-  '<h2 id="firstHeading" class="firstHeading">Landing site</h2>'+
-  '<div id="bodyContent">'+
-  '<p>The aliens landed somewhere out in the desert...!</p>'+
-  
-  '</div>'+
-  '</div>';
+    function goTo(div,location) {
 
 
-  var contentString3 = '<div id="content">'+
-  '<div id="siteNotice">'+
-  '</div>'+
-  '<h2 id="firstHeading" class="firstHeading">KTH</h2>'+
-  '<div id="bodyContent">'+
-  '<p>hmmm...Aliens??</p>'+
-  
-  '</div>'+
-  '</div>'; 
+      google.maps.event.addDomListener(div, 'click', function() {
+        var newlocation = location;
+        map.setCenter(newlocation);
+        map.setZoom(13);
+      });
+    }
+      function goHome() {
+        var Home = new google.maps.LatLng(37.236752, -115.801813);
+        map.setCenter(Home);
+      }
 
+      function goKTH() {
+        var Home = new google.maps.LatLng(59.34694918843402, 18.07322323322296);
+        map.setCenter(Home);
+      }
 
-  var infowindow1 = new google.maps.InfoWindow({
-    content: contentString1,
-    maxWidth: 200
-  });
+    
+    function zoomin() {
+     var currentZoomLevel = map.getZoom();
+     if(currentZoomLevel != 0){
+       map.setZoom(currentZoomLevel + 1);}   
+     }
 
-  var infowindow2 = new google.maps.InfoWindow({
-    content: contentString2,
-    maxWidth: 200
-  });
+     function zoomout() {
+       var currentZoomLevel = map.getZoom();
+       if(currentZoomLevel != 0){
+         map.setZoom(currentZoomLevel - 1);}   
+       }
 
-  var infowindow3 = new google.maps.InfoWindow({
-    content: contentString3,
-    maxWidth: 200
-  });
+       function goUp() {
+         var oldLoc = map.getCenter();
+         var newLoc = new google.maps.LatLng(oldLoc.lat() + 0.05, oldLoc.lng());
+         map.setCenter(newLoc);   
+       }
 
-  var marker1 = new google.maps.Marker({
-    position: Area51,
-    map: map,
-    draggable: false,
-    animation: google.maps.Animation.DROP,
-    title: "Area51"
-  });
+       function goDown() {
+         var oldLoc = map.getCenter();
+         var newLoc = new google.maps.LatLng(oldLoc.lat() - 0.05, oldLoc.lng());
+         map.setCenter(newLoc);   
+       }
 
+       function goLeft() {
+         var oldLoc = map.getCenter();
+         var newLoc = new google.maps.LatLng(oldLoc.lat(), oldLoc.lng() - 0.05);
+         map.setCenter(newLoc);   
+       }
 
-  var marker2 = new google.maps.Marker({
-    position: UFO,
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.BOUNCE,
-    title: "UFO Landing site"
-  });
-
-  var marker3 = new google.maps.Marker({
-    map:map,
-    draggable:true,
-    animation: google.maps.Animation.DROP,
-    position: KTH,
-    title: "KTH"
-  });
-console.log(geo);
-  var marker4 = new google.maps.Marker({
-    map:map,
-    draggable:true,
-    animation: google.maps.Animation.DROP,
-    position: geo,
-    title: "myPosition"
-  });
-
-  google.maps.event.addListener(marker1, 'click', function() {
-    infowindow1.open(map,marker1);
-  })
-  google.maps.event.addListener(marker2, 'click', function() {
-    infowindow2.open(map,marker2);
-  })
-
-  google.maps.event.addListener(marker3, 'click', function() {
-    infowindow3.open(map,marker3);
-  })      
+       function goRight() {
+         var oldLoc = map.getCenter();
+         var newLoc = new google.maps.LatLng(oldLoc.lat(), oldLoc.lng() + 0.05);
+         map.setCenter(newLoc);   
+       }
 
 
 
+       function initialize() {
+        var mapOptions = {
+          zoom: 13,
+          center: Area51,
+          mapTypeId: 'satellite',
+          disableDefaultUI: true,
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.LEFT_TOP
+          }
+        }
 
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+
+
+        var contentString1 = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h2 id="firstHeading" class="firstHeading">Area51</h2>'+
+        '<div id="bodyContent">'+
+        '<p>Aliens where here!</p>'+
+
+        '</div>'+
+        '</div>';
+
+        var contentString2 = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h2 id="firstHeading" class="firstHeading">Landing site</h2>'+
+        '<div id="bodyContent">'+
+        '<p>The aliens landed somewhere out in the desert...!</p>'+
+
+        '</div>'+
+        '</div>';
+
+
+        var contentString3 = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h2 id="firstHeading" class="firstHeading">KTH</h2>'+
+        '<div id="bodyContent">'+
+        '<p>hmmm...Aliens??</p>'+
+
+        '</div>'+
+        '</div>'; 
+
+
+        var infowindow1 = new google.maps.InfoWindow({
+          content: contentString1,
+          maxWidth: 200
+        });
+
+        var infowindow2 = new google.maps.InfoWindow({
+          content: contentString2,
+          maxWidth: 200
+        });
+
+        var infowindow3 = new google.maps.InfoWindow({
+          content: contentString3,
+          maxWidth: 200
+        });
+
+        var marker1 = new google.maps.Marker({
+          position: Area51,
+          map: map,
+          draggable: false,
+          animation: google.maps.Animation.DROP,
+          title: "Area51"
+        });
+
+
+        var marker2 = new google.maps.Marker({
+          position: UFO,
+          map: map,
+          draggable: true,
+          animation: google.maps.Animation.BOUNCE,
+          title: "UFO Landing site"
+        });
+
+        var marker3 = new google.maps.Marker({
+          map:map,
+          draggable:true,
+          animation: google.maps.Animation.DROP,
+          position: KTH,
+          title: "KTH"
+        });
+        
+
+
+        google.maps.event.addListener(marker1, 'click', function() {
+          infowindow1.open(map,marker1);
+        })
+        google.maps.event.addListener(marker2, 'click', function() {
+          infowindow2.open(map,marker2);
+        })
+
+        google.maps.event.addListener(marker3, 'click', function() {
+          infowindow3.open(map,marker3);
+        })      
+
+
+
+
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
