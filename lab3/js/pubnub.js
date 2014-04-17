da133041e0b89c3a191efc2a4bbb81527752da99
roshult <roshult89@gmail.com>
2014-04-18 00:51:17 +0200

    no message

--- lab3/js/pubnub.js
+++ lab3/js/pubnub.js
@@ -1,26 +1,16 @@
-
-    function start() {
- 
-
-      // Grab the elements
-      var input = $("#input");
-      var username = $("#username");
-      var channel = "Other";
-      var buttonSend = $("#buttonSend");
-      var buttonHistory = $("#buttonHistory");
-      var output = $("#output");
-      
-       // Init PubNub
-    	console.log("Tommy");
-		var pubnub = PUBNUB.init({
+     var i = 1;
+     
+    var input = "Chatten börjar ....";
+    var username = "Server";
+    var channel = "ingen";
+     
+    var pubnub = PUBNUB.init({
         publish_key   : "pub-c-273ee5c6-cded-4915-b196-954c4fc1ba4e",
         subscribe_key : "sub-c-39034c9e-c3a8-11e3-ab7b-02ee2ddab7fe",
         uuid: 'Evert'
-        
-      });
-
-      // send messages
-        if(navigator.geolocation) {
+        })
+ 
+ 	if(navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
            console.log("Hej");
            var lat = position.coords.latitude;
@@ -58,32 +48,69 @@
             channel = "D-huset";
           } else {
             channel = "Other";
-            if (input.val() != '' && username.val != ('')){
-            Communication(channel, username, input, output, pubnub)
-          }}})}};
-          
-    function Communication(channel, username, input, output, pubnub){
+           }})}
+           
+         
     
-   
+     
 
+    function start() {
 
+      // Grab the elements
+       input = $("#input");
+       username = $("#username");
+       channel = "Other";
+      var buttonSend = $("#buttonSend");
+      var buttonHistory = $("#buttonHistory");
+      var output = $("#output");
+     
+       if (input.val() != '' && username.val != ('')){
+       	
+            Communication(channel, username, input, output)
+            Communication(channel, username, input, output)
+            Communication(channel, username, input, output)
+          
+          }
+      };
+          
+    function Communication(channel, username, input, output){
+    	 
+    	  var text = output.html();
+    	 
+    	  
+    	  pubnub.publish({
+            'channel' : channel,
+            'message' : "<br /><b>" + username.val() + " says:</b><br />" + input.val() + "<br />"
+			 
+          })
+          
+         
           pubnub.subscribe({
            'channel'   : channel,
            'callback'  : function(message) {
-            console.log('Channel Change = ' + channel )
-            output.html(output.html() + '<br />' + "Channel: " + channel + message);
+            output.html(text + '<br />' + "Channel: " + channel + message);
             input.val('');
           }
-        });
-        
-          pubnub.publish({
-
+          
+        })
+        if (text==""){
+         pubnub.publish({
             'channel' : channel,
             'message' : "<br /><b>" + username.val() + " says:</b><br />" + input.val() + "<br />"
-
-          });
-       
+			 
+          })
+          
+         
+          pubnub.subscribe({
+           'channel'   : channel,
+           'callback'  : function(message) {
+            output.html(text + '<br />' + "Channel: " + channel + message);
+            input.val('');
           }
+          
+        })
+        }
+        }
 
 
 
