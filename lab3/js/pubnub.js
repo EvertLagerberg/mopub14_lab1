4f51eb7806ca3237ccf8e7fadd4e660359c9cc15
roshult <roshult89@gmail.com>
2014-04-17 22:18:13 +0200

    no message

--- lab3/js/pubnub.js
+++ lab3/js/pubnub.js
@@ -2,6 +2,7 @@
 
     // When the DOM is ready...
     $(function() {
+ 
 
       // Grab the elements
       var input = $("#input");
@@ -10,19 +11,6 @@
       var buttonSend = $("#buttonSend");
       var buttonHistory = $("#buttonHistory");
       var output = $("#output");
-
-      // Init PubNub
-      var pubnub = PUBNUB.init({
-        publish_key   : "pub-c-273ee5c6-cded-4915-b196-954c4fc1ba4e",
-        subscribe_key : "sub-c-39034c9e-c3a8-11e3-ab7b-02ee2ddab7fe",
-        uuid: 'Evert'
-      });
-
-
-      
-
-      
-      
 
       // send messages
       buttonSend.on('click', function() {
@@ -61,38 +49,44 @@
 
             ) 
            {
-            console.log("Hej");
-            channel = "torget";
+            channel = "D-huset";
           } else {
             channel = "Other";
-          }
+            if (input.val() != '' && username.val() != ''){
+            console.log("Tommy");
+            Communication(channel, username, input, output)
+          }}})}})});
+          
+    function Communication(channel, username, input, output){
+    
+    // Init PubNub
+    
+      var pubnub = PUBNUB.init({
+        publish_key   : "pub-c-273ee5c6-cded-4915-b196-954c4fc1ba4e",
+        subscribe_key : "sub-c-39034c9e-c3a8-11e3-ab7b-02ee2ddab7fe",
+        uuid: 'Evert'
+        
+      });
+
 
           pubnub.subscribe({
            'channel'   : channel,
            'callback'  : function(message) {
             console.log('Channel Change = ' + channel )
             output.html(output.html() + '<br />' + "Channel: " + channel + message);
+            input.val('');
           }
         });
-
-
-
-
-
-
-          console.log(channel);
-
-
+        
           pubnub.publish({
 
             'channel' : channel,
-            'message' : "<br /><b>" + username.val() + " says:</b><br />" + input.val() + "<br />" + nav
+            'message' : "<br /><b>" + username.val() + " says:</b><br />" + input.val() + "<br />"
 
           });
+       
+          }
 
-
-        });
-}})});
 
 
 
