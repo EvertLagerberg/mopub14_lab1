eda672c9fcb2f11f943ce5a258208527cc71a89f
EvertLagerberg <evert.lagerberg@gmail.com>
2014-04-23 11:43:36 +0200

    no message

--- lab4/js/add.js
+++ lab4/js/add.js
@@ -1,6 +1,6 @@
 
 
-
+buttonSend.on('click', function() {
 // First initialise the helper object with the code, secret code 
 // and the generic helper
 var helper = new CBHelper("lab5", "0123456789abcdef0123456789", new GenericHelper());
@@ -10,9 +10,12 @@ helper.setPassword(hex_md5("smoothbuckle12"));
 // create an object to be inserted in our CloudBase instance,
 // e.g a firstName, lastName and title into a collection called ‘users’
 var dataObject = {
-    "firstName" : "Niklas",
-    "lastName" : "Nummelin",
-    "title" : "Wizard"
+    "Name" : "Evert",
+    "Desc" : "Evert",
+    "Category" : "Evert",
+    "Location" : "Evert",
+    "price-range" : "Evert",
+    "image" : ""
 };
  // use the insertDocument method to send the call and insert the data
 // in the users collection then print out the response using the
@@ -21,3 +24,4 @@ var dataObject = {
 helper.insertDocument("users", dataObject, null, function(resp) {
     hyper.log(resp.outputString);
 });
+})
